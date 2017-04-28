84bbe62d091d42a4ed6450f1c97b5e7b27a822a9
Nolan Jiang <scargtt@163.com>
2017-04-28 13:49:47 +0800

    init

--- bfs.js
+++ bfs.js
@@ -1,4 +1,3 @@
-
 let mazeData = [
     [1, 0, 0, 0, 0],
     [1, 1, 0, 1, 0],
@@ -7,73 +6,97 @@ let mazeData = [
     [0, 0, 0, 0, 1]
 ];
 
-for (let i = mazeData.length - 1; i >= 0; i--) {
-    for (let j = mazeData[i].length - 1; j >= 0; j--) {
-        mazeData[i][j] = {
-            value: mazeData[i][j],
-            isVisited: false,
-            x: i,
-            y: j
+function bfs(mazeData) {
+
+
+    for (let i = mazeData.length - 1; i >= 0; i--) {
+        for (let j = mazeData[i].length - 1; j >= 0; j--) {
+            mazeData[i][j] = {
+                value: mazeData[i][j],
+                isVisited: false,
+                x: i,
+                y: j
+            }
         }
     }
 
-}
+    let res = [];
 
-let res = [];
+    let node = mazeData[4][4]// 遍历每个节点
 
-
-for (let i = mazeData.length - 1; i >= 0; i--) {
-    for (let j = mazeData[i].length - 1; j >= 0; j--) {
-
-        let node = mazeData[i][j]// 遍历每个节点
+    while (node) {
+        // console(a)
         let queue = []; // 辅助队列
 
         if (!node.isVisited) {
-
             node.isVisited = true;
+            // console.log(node)
             queue.unshift(node); // 入队
-
             while (queue.length) { // 队列非空
-
+                // console.log(queue.length);
                 let u = queue.shift(); // 队首元素 出队
                 // console.log(u);
-
                 u.neighbor = [];
-
-                if (mazeData[i - 1] && mazeData[i - 1][j].value) {// 上
-                    u.neighbor.push(mazeData[i - 1][j]);
+                if (mazeData[node.x - 1] && mazeData[node.x - 1][node.y].value) {// 上
+                    if (!mazeData[node.x - 1][node.y].isVisited)
+                        u.neighbor.push(mazeData[node.x - 1][node.y]);
+                }
+                if (mazeData[node.x][node.y + 1] && mazeData[node.x][node.y + 1].value) {// 右
+                    if (!mazeData[node.x][node.y + 1].isVisited)
+                        u.neighbor.push(mazeData[node.x][node.y + 1]);
+                }
+                if (mazeData[node.x + 1] && mazeData[node.x + 1][node.y].value) {// 下
+                    if (!mazeData[node.x + 1][node.y].isVisited)
+                        u.neighbor.push(mazeData[node.x + 1][node.y]);
+                }
+                if (mazeData[node.x][node.y - 1] && mazeData[node.x][node.y - 1].value) {// 左
+                    if (!mazeData[node.x][node.y - 1].isVisited)
+                        u.neighbor.push(mazeData[node.x][node.y - 1]);
                 }
 
-                if (mazeData[i + 1] && mazeData[i + 1][j].value) {// 下
-                    u.neighbor.push(mazeData[i + 1][j]);
-                }
-
-                if (mazeData[i][j - 1] && mazeData[i][j - 1].value) {// 左
-                    u.neighbor.push(mazeData[i][j - 1]);
-                }
-
-                if (mazeData[i][j + 1] && mazeData[i][j + 1].value) {// 右
-                    u.neighbor.push(mazeData[i][j + 1]);
-                }
-
+                // console.log(u.neighbor);
+                // 遍历邻居节点
                 for (neighborNode of u.neighbor) {
                     if (!neighborNode.isVisited) {
                         neighborNode.isVisited = true;
-                        // console.log(neighborNode);
-                        res.push({
+
+                        /*res.push({
+                            row: node.x,
+                            col: node.y,
                             x: neighborNode.x,
                             y: neighborNode.y
 
-                        })
-                        queue.unshift(neighborNode);
+                        });*/
+                        queue.push(neighborNode);
                     }
-                }
-            }
-        }
-
+                }// for 遍历邻居节点
+                // console.log(queue[0]);
+                node = queue[0];
+                res.push(node);
+                // console.log(node);
+            }// while
+        }// visited
     }
 
+    return res
 }
 
 
-console.log(res);
+let pathTree = bfs(mazeData)[0];
+
+console.log(pathTree);
+
+function printPathTree(pathTree) {
+    var str ='';
+    function getChild(tree) {
+        if(!tree){
+            //  return
+        } else {
+            getChild(tree.left);
+            getChild(tree.right);
+            str = str + '->' + tree.value;
+            return str;
+        }
+    }
+}
+
